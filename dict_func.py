3bc900e9309a646022ec56ff7734af41d37fc884
Aleja <paulagarro@unisabana.edu.co>
2026-04-07 21:52:24 -0500

    p 3

--- dict_func.py
+++ dict_func.py
@@ -1,1 +1,17 @@
 # Write your code here!
+def employee_print(employee_info):
+    info = employee_info.copy()
+
+    name   = info.pop("Name",   "N/A")
+    salary = info.pop("Salary", "N/A")
+    role   = info.pop("Role",   "N/A")
+
+    print(f"Name: {name}")
+    print(f"Salary: {salary}")
+    print(f"Role: {role}")
+
+    if info:
+        for key, value in info.items():
+            print(f"{key}: {value}")
+    else:
+        print("No other info!")
